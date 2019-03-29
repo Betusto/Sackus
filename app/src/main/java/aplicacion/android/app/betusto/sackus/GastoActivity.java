c19e968a54a66427f2b60f218664a49664f518bc
Betusto <44010738+Betusto@users.noreply.github.com>
2019-03-29 04:20:13 -0600

    Se crearon nuevos activities y se les pusieron algunos elementos a algunos, falta hacer testeo

--- app/src/main/java/aplicacion/android/app/betusto/sackus/GastoActivity.java
+++ app/src/main/java/aplicacion/android/app/betusto/sackus/GastoActivity.java
@@ -19,6 +19,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
 import android.view.ViewGroup;
 
 import android.widget.Button;
